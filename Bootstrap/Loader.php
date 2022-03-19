32782b6a8702d2b0731cb1e7694511f6582e1f10
aliNejati-bu <electrocellco@gmail.com>
2022-03-19 16:57:44 +0330

    register DIC

--- Bootstrap/Loader.php
+++ Bootstrap/Loader.php
@@ -2,16 +2,25 @@
 
 namespace Bootstrap;
 
+use Electro\Bootstrap\DicHandler;
+
 class Loader
 {
     public static function BOOT()
     {
-        spl_autoload_register(function ($class){
-            $class = str_replace('Electro\\',"",$class);
+        spl_autoload_register(function ($class) {
+            $class = str_replace('Electro\\', "", $class);
             $file = ELECTRO_BASE . DIRECTORY_SEPARATOR .
-                str_replace("\\",DIRECTORY_SEPARATOR,$class). // replace \ in class namespace to DIRECTORY_SEPARATOR
+                str_replace("\\", DIRECTORY_SEPARATOR, $class) . // replace \ in class namespace to DIRECTORY_SEPARATOR
                 ".php";
             require_once $file; // include if file exists.
         });
+        self::loadDIC();
+    }
+
+    public static function loadDIC()
+    {
+        DicHandler::Register();
+        DicHandler::addGroup(require_once ELECTRO_BASE . DIRECTORY_SEPARATOR . "Bootstrap" . DIRECTORY_SEPARATOR . "Providers" . DIRECTORY_SEPARATOR . "define.php");
     }
 }
