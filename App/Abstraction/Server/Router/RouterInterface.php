121424d563d265d14d8103e5ef261e28381bde2f
aliNejati-bu <electrocellco@gmail.com>
2022-03-19 07:44:46 +0330

    add RouterInterface

--- App/Abstraction/Server/Router/RouterInterface.php
+++ App/Abstraction/Server/Router/RouterInterface.php
@@ -4,5 +4,60 @@ namespace Electro\App\Abstraction\Server\Router;
 
 interface RouterInterface
 {
+    /**
+     * @param string $pattern
+     * @param callable $handler function (req,res)
+     * @param array $middlewares
+     * for add get route
+     */
+    public function GET(string $pattern, callable $handler, array $middlewares): void;
+
+    /**
+     * @param string $pattern
+     * @param callable $handler function (req,res)
+     * @param array $middlewares
+     * for add post route
+     */
+    public function POST(string $pattern, callable $handler, array $middlewares): void;
+
+    /**
+     * @param string $pattern
+     * @param callable $handler function (req,res)
+     * @param array $middlewares
+     * for add put route
+     */
+    public function PUT(string $pattern, callable $handler, array $middlewares): void;
+
+    /**
+     * @param string $pattern
+     * @param callable $handler function (req,res)
+     * @param array $middlewares
+     * for add patch route
+     */
+    public function PATCH(string $pattern, callable $handler, array $middlewares): void;
+
+    /**
+     * @param string $pattern
+     * @param callable $handler function (req,res)
+     * @param array $middlewares
+     * for add delete route
+     */
+    public function DELETE(string $pattern, callable $handler, array $middlewares): void;
+
+    /**
+     * for run routing
+     * @return void
+     */
+    public function run(): void;
+
+    /**
+     * @param string $prefix
+     * @param array $middlewares
+     * @param callable $handler
+     * for add prefix to group of routs.
+     */
+    public function group(string $prefix,array $middlewares,callable $handler): void;
+
+
 
 }
